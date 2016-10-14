1f178eb68d932fcf78054c1c1c4a742fe7d48527
jinimp <linjinquan14@163.com>
2016-10-14 21:07:16 +0800

    Signed-off-by: jinimp <linjinquan14@163.com>

--- APP-1/server.R
+++
@@ -1,6 +0,0 @@
-server<- shinyServer(function(input,output){
-output$textDisplay <- renderText(
-  paste0("You said ", input$comment," there are",
-         nchar(input$comment)," charater in this.")
-)
-})
